3b6bd07f39d1d8bd5066b423af8c4c316b7ab09b
salheenjr <salheenalagore@gmail.com>
2023-08-11 06:07:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, quo, 59);
+
+	return (1);
+}
