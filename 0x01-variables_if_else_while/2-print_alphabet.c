b98bf3c9779651a3129d79238d62400951dcf8fa
salheenjr <salheenalagore@gmail.com>
2023-08-11 18:07:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet using putchar function
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char chr = 'a';
+
+	while (chr <= 'z')
+	{
+		putchar(ch);
+		chr++;
+	}
+	putchar("\n");
+
+	return (0);
+}
