46d6fa24e6bd9680a60ad40ea23eaed6943c297e
salheenjr <salheenalagore@gmail.com>
2023-08-16 00:35:35 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,10 +11,9 @@
 
 int _isalpha(int c)
 {
-	int uppercase = (c >= 65 && c <= 90);
-	int lowercase = (c >= 97 && c <= 122);
-
-	if (uppercase == 0 || lowercase == 0)
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
