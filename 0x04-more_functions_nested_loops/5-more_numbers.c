488437eeb2751056bc8afcc14e46adf9257be4b5
salheenjr <salheenalagore@gmail.com>
2023-08-17 11:33:57 -0700

    5-more_numbers.c modified 03

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,9 +16,12 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n);
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
