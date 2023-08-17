d34c652a6cec9f4884f708776f16b5a47ea60755
salheenjr <salheenalagore@gmail.com>
2023-08-17 08:20:42 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is uppercase
+ *
+ * @c: charcter input
+ *
+ * Return: 1 if c is uppercase
+ * otherwise 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is ReadMe File for 0x04-more_functions_nested_loops Task
