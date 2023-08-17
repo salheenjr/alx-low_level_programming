f029eb05cdb4c1f1a4988863696fb92c515b7436
salheenjr <salheenalagore@gmail.com>
2023-08-17 11:47:41 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ *
+ * @n: number input
+ *
+ * Return: Alwaya 0 (success)
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+			putchar(95);
+		putchar('\n');
+	}
+}
