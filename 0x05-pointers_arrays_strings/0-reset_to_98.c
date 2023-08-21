63f2e55f0859390c8e089658bcfe525be63c5153
salheenjr <salheenalagore@gmail.com>
2023-08-21 08:19:28 -0700

    Creat new dir 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ *
+ * @n: pointer input
+ *
+ * Return: Always 0 (success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
